59874ee1a4169d6a4800085687bd34aa543ed9c1
Ethan Samsioe Sarras <ethan.samsioesarra@gmail.com>
2026-04-15 00:01:32 +0200

    Added Basic Healing and Damage
    Added
    -----------------------------------------------------
    +DamageSystemComponent.cpp & .h
    
    Any character that is a child of the BP_CharacterBase
    now has the base to take damage and heal

---
+++ Untitled_Game/Source/Untitled_Game/DamageSystem/DamageSystemComponent.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DamageSystemComponent.h"
+
+
+// Sets default values for this component's properties
+UDamageSystemComponent::UDamageSystemComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UDamageSystemComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+bool UDamageSystemComponent::HandleIncomingDamage(const FDamageInfo& DamageInfo)
+{
+	if (IsDead){ return false; }
+	
+	//if (IsBlocking && DamageInfo.CanBeLocked) { return false; }
+	
+	CurrentHealth = FMath::Clamp(CurrentHealth - DamageInfo.DamageAmount, 0.f, MaxHealth);
+	
+	return true;
+}
+
+void UDamageSystemComponent::HandleIncomingHeal(float HealAmount, AActor* Healer)
+{
+	if (IsDead){ return; }
+	
+	CurrentHealth = FMath::Clamp(CurrentHealth + HealAmount, 0.f, MaxHealth);
+}
+
---
+++ Untitled_Game/Source/Untitled_Game/DamageSystem/DamageSystemComponent.h
@@ -0,0 +1,55 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DamageSystemTypes.h"
+#include "Components/ActorComponent.h"
+#include "DamageSystemComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class UNTITLED_GAME_API UDamageSystemComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UDamageSystemComponent();
+	
+	UPROPERTY()
+	float MaxHealth = 100.0f;
+	
+private:
+	UPROPERTY()
+	float CurrentHealth = MaxHealth;
+	
+	UPROPERTY()
+	bool IsDead = false;
+	
+	//bool IsBlocking?
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Damage")
+	bool HandleIncomingDamage(const FDamageInfo& DamageInfo);
+	
+	UFUNCTION(BlueprintCallable, Category = "Damage")
+	void HandleIncomingHeal(float HealAmount, AActor* Healer);
+	
+	// GETTER FUNCTIONS //
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Health")
+	float GetCurrentHealth() { return CurrentHealth; };
+	
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Health")
+	float GetMaxHealth() const { return MaxHealth; };
+	
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Health")
+	bool GetIsDead() const { return IsDead; };
+	
+	// GETTER FUNCTIONS //
+	// Set IsBlocking...
+};
