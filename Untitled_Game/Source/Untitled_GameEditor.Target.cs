5719009ae121d0a9c6e903667231127a8fba224f
Robbzn <rba@live.se>
2026-04-15 21:59:43 +0200

    Updated Project
    Updated the project from Unreal 5.6 to Unreal 5.7

--- Untitled_Game/Source/Untitled_GameEditor.Target.cs
+++ Untitled_Game/Source/Untitled_GameEditor.Target.cs
@@ -8,7 +8,7 @@ public class Untitled_GameEditorTarget : TargetRules
 	public Untitled_GameEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 
 		ExtraModuleNames.AddRange( new string[] { "Untitled_Game" } );
 	}
