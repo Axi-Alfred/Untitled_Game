30aeddd97779bc14ea37c81fe40a820b3ed494df
Axi-Alfred <181472998+Axi-Alfred@users.noreply.github.com>
2026-04-15 21:20:02 +0200

    5.7.4

--- Untitled_Game/Source/Untitled_Game.Target.cs
+++ Untitled_Game/Source/Untitled_Game.Target.cs
@@ -8,7 +8,7 @@ public class Untitled_GameTarget : TargetRules
 	public Untitled_GameTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 
 		ExtraModuleNames.AddRange( new string[] { "Untitled_Game" } );
 	}
