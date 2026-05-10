2c03eaef22bc0e5983257e0800ccdd0944c08066
Ethan Samsioe Sarras <ethan.samsioesarra@gmail.com>
2026-05-10 22:05:19 +0200

    Added base for objectives and Healing spell

---
+++ Untitled_Game/Source/Untitled_Game/Enemies/Objective.cpp
@@ -0,0 +1,80 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Objective.h"
+
+
+// Sets default values
+AObjective::AObjective()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AObjective::BeginPlay()
+{
+	Super::BeginPlay();
+	GrantAbilities(StartingAbilities);
+	GrantAbilities(StartingAbilities);
+	AbilitySystemComponent->RegisterGameplayTagEvent(FGameplayTag::RequestGameplayTag("State.Dead")
+		).AddUObject(this, &AObjective::OnDeadTagChanged);
+	
+}
+
+void AObjective::OnDeadTagChanged(const FGameplayTag CallBackTag, int32 NewCount)
+{
+	if (NewCount > 0)
+	{
+		HandleDeath();
+	}
+}
+
+void AObjective::HandleDeath_Implementation()
+{
+}
+
+// Called every frame
+void AObjective::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+UAbilitySystemComponent* AObjective::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+TArray<FGameplayAbilitySpecHandle> AObjective::GrantAbilities(TArray<TSubclassOf<UGameplayAbility>> AbilitiesToGrant)
+{
+	
+	if (!AbilitySystemComponent || !HasAuthority())
+	{
+		return TArray<FGameplayAbilitySpecHandle>();
+	}
+	
+	TArray<FGameplayAbilitySpecHandle> AbilityHandles;
+	
+	for (TSubclassOf<UGameplayAbility> Ability : AbilitiesToGrant)
+	{
+		FGameplayAbilitySpecHandle SpecHandle = AbilitySystemComponent->GiveAbility(FGameplayAbilitySpec
+			(Ability, 1, -1, this));
+		AbilityHandles.Add(SpecHandle);
+	}
+	
+	return AbilityHandles;
+}
+
+void AObjective::RemoveAbilities(TArray<FGameplayAbilitySpecHandle> AbilityHandlesToRemove)
+{
+	if (!AbilitySystemComponent || !HasAuthority())
+	{
+		return;
+	}
+	
+	for (FGameplayAbilitySpecHandle AbilityHandle : AbilityHandlesToRemove)
+	{
+		AbilitySystemComponent->ClearAbility(AbilityHandle);
+	}
+}
+
