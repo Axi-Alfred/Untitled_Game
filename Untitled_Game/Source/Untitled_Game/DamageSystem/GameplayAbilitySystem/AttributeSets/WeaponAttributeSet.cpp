0c1fc59febcda9bf6900f11cf982a4527d953b83
Ethan Samsioe Sarras <ethan.samsioesarra@gmail.com>
2026-05-26 13:29:29 +0200

    Weapon Attributes script completed

--- Untitled_Game/Source/Untitled_Game/DamageSystem/GameplayAbilitySystem/AttributeSets/WeaponAttributeSet.cpp
+++ Untitled_Game/Source/Untitled_Game/DamageSystem/GameplayAbilitySystem/AttributeSets/WeaponAttributeSet.cpp
@@ -2,3 +2,69 @@
 
 
 #include "WeaponAttributeSet.h"
+#include "GameplayEffectExtension.h"
+#include "Net/UnrealNetwork.h"
+
+UWeaponAttributeSet::UWeaponAttributeSet()
+{
+	Range = 600.f;
+	Damage = 25.f;
+	Healing = 25.f;
+	AttackSpeed = 2.f;
+}
+
+void UWeaponAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME_CONDITION_NOTIFY(UWeaponAttributeSet, Range, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UWeaponAttributeSet, Damage, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UWeaponAttributeSet, Healing, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UWeaponAttributeSet, AttackSpeed, COND_None, REPNOTIFY_Always);
+}
+
+void UWeaponAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+	
+	if (Attribute == GetDamageAttribute())
+	{
+		if (NewValue > 0.f)
+		{
+			NewValue *= -1.f;
+		}
+	}
+	
+	if (Attribute == GetHealingAttribute())
+	{
+		if (NewValue < 0.f)
+		{
+			NewValue *= -1.f;
+		}
+		
+	}
+}
+
+void UWeaponAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& ExecData)
+{
+	Super::PostGameplayEffectExecute(ExecData);
+	
+	if (ExecData.EvaluatedData.Attribute == GetRangeAttribute())
+	{
+		SetRange(GetRange());
+	}
+	
+	if (ExecData.EvaluatedData.Attribute == GetDamageAttribute())
+	{
+		SetDamage(GetDamage());
+	}
+	
+	if (ExecData.EvaluatedData.Attribute == GetHealingAttribute())
+	{
+		SetHealing(GetHealing());
+	}
+	
+	if (ExecData.EvaluatedData.Attribute == GetRangeAttribute())
+	{
+		SetAttackSpeed(GetAttackSpeed());
+	}
+}
--- Untitled_Game/Source/Untitled_Game/DamageSystem/GameplayAbilitySystem/AttributeSets/WeaponAttributeSet.h
+++ Untitled_Game/Source/Untitled_Game/DamageSystem/GameplayAbilitySystem/AttributeSets/WeaponAttributeSet.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "WeaponAttributeSet.generated.h"
 
 /**
@@ -13,4 +14,54 @@ UCLASS()
 class UNTITLED_GAME_API UWeaponAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+	
+public:
+	UWeaponAttributeSet();
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes", ReplicatedUsing=OnRep_Range)
+	FGameplayAttributeData Range;
+	ATTRIBUTE_ACCESSORS_BASIC(UWeaponAttributeSet, Range);
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes", ReplicatedUsing=OnRep_Damage)
+	FGameplayAttributeData Damage;
+	ATTRIBUTE_ACCESSORS_BASIC(UWeaponAttributeSet, Damage);
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes", ReplicatedUsing=OnRep_Healing)
+	FGameplayAttributeData Healing;
+	ATTRIBUTE_ACCESSORS_BASIC(UWeaponAttributeSet, Healing);
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes", ReplicatedUsing=OnRep_AttackSpeed)
+	FGameplayAttributeData AttackSpeed;
+	ATTRIBUTE_ACCESSORS_BASIC(UWeaponAttributeSet, AttackSpeed);
+	
+public:
+	UFUNCTION()
+	void OnRep_Range(const FGameplayAttributeData& oldValue) const
+	{
+		GAMEPLAYATTRIBUTE_REPNOTIFY(UWeaponAttributeSet, Range, oldValue);
+	}
+	
+	UFUNCTION()
+	void OnRep_Damage(const FGameplayAttributeData& oldValue) const
+	{
+		GAMEPLAYATTRIBUTE_REPNOTIFY(UWeaponAttributeSet, Damage, oldValue);
+	}
+	
+	UFUNCTION()
+	void OnRep_Healing(const FGameplayAttributeData& oldValue) const
+	{
+		GAMEPLAYATTRIBUTE_REPNOTIFY(UWeaponAttributeSet, Healing, oldValue);
+	}
+	
+	UFUNCTION()
+	void OnRep_AttackSpeed(const FGameplayAttributeData& oldValue) const
+	{
+		GAMEPLAYATTRIBUTE_REPNOTIFY(UWeaponAttributeSet, AttackSpeed, oldValue);
+	}
+	
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	
+	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& ExecData) override;
 };
