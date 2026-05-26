414e90b238d2597bb09d1d94aea33df7174d876e
Ethan Samsioe Sarras <ethan.samsioesarra@gmail.com>
2026-05-26 12:58:06 +0200

    Added scripts for Weapon Attributes

---
+++ Untitled_Game/Source/Untitled_Game/DamageSystem/GameplayAbilitySystem/AttributeSets/WeaponAttributeSet.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "WeaponAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNTITLED_GAME_API UWeaponAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+};
